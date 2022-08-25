0cd175985e193c2907980da55926174e19654bd9
DanielQueijeiro <111073727+DanielQueijeiro@users.noreply.github.com>
2022-08-25 16:49:21 -0500

    corrección de errores en código

--- ahorcado.py
+++ ahorcado.py
@@ -9,23 +9,21 @@ lista = ["tarjeta", "pepinillo", "bocina", "codigo", "profesional"]
 palabra = lista[(int(input()))]
 #mostrar cuantas letras tiene su palabra
 print("Su palabra contiene", len(palabra), "letras")
+print("Al alcanzar 6 errores, pierdes")
 #print(palabra)
 #set errores a 0
-e = 0
+e = 1
 #adivinar y suma de errores (7 intentos)
+l = len(palabra)
 while e < 7:
     print("Escriba una letra para adivinar")
     if str(input()) in palabra:
-        print("Correcto")
+        print("Correcto, la palabra es:", l )
     else:
-        print(f"Incorrecto, tiene {e + 1} error/es")
+        print(f"Incorrecto, tiene {e} error/es")
         e = e + 1
-print("Perdiste")
+#perder
+print("Has alcanzado el maximo número de errores, perdiste")
 
 
 #ganar
-
-
-#perder
-if e == 6:
-    print("Ha alcanzadado el maximo numero de errores")
