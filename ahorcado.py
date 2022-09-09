939f9cc00c10a72e850d5ad27db62244ba3e01b2
DanielQueijeiro <111073727+DanielQueijeiro@users.noreply.github.com>
2022-09-08 23:26:08 -0500

    Add files via upload

--- ahorcado.py
+++ ahorcado.py
@@ -1,5 +1,10 @@
+from dataclasses import replace
 import random
 from time import sleep
+
+
+
+
 
 
 #bienvenida al juego
@@ -7,15 +12,17 @@ def bienvenida_juego():
     print("Bienvenido a Ahorcado, hecho por Daniel Queijeiro")
     sleep(1)
 
+
 bienvenida_juego()
 #lista de palabras
 lista = ["tarjeta", "pepinillo", "bocina", "codigo", "profesional"]
-palabra = lista[random.randint(0,4)]
+diccionario = lista[random.randint(0,4)]
+diccionario = list(diccionario)
+print(diccionario)
+tablero=len(diccionario)*"_"
 
 #mostrar cuantas letras tiene su palabra
-print("Su palabra contiene", len(palabra), "letras")
-palabra2 = palabra
-palabra2 = list(palabra2)
+print("Su palabra contiene", "_" * len(diccionario), len(diccionario), "letras")
 sleep(1)
 print("Al alcanzar 6 errores, pierdes")
 sleep(1)
@@ -24,23 +31,26 @@ sleep(1)
 e = 1
 
 #adivinar y suma de errores (6 intentos)
-l = len(palabra)
 while e < 7:
     print("Escriba una letra para adivinar/ Si cree saber cual es la palabra, introduzcala")
     i = str((input()))
-    if i in palabra:
-        for index, value in enumerate(palabra2):
-            if value != i:
-                palabra2[index] = "_"
-        print("Correcto, la palabra es:", palabra2)
+    if diccionario == tablero:
+        print("ganaste")
+    elif "a" <= i <="z" and len(i) ==1:
+        if i in diccionario:
+            for index, letra_palabra in enumerate(diccionario):
+                if i == letra_palabra:
+                    tablero[index] = i
+            print("Correcto, la palabra es:", tablero)
+        elif i not in diccionario:
+                print(f"Incorrecto, tiene {e} error/es")
+                e = e + 1
+        
+        if i == diccionario:#ganar///como checar que la palabra ya este completa y proceder a ganar
+            print("Correcto, la palabra es:", diccionario)
+            quit()
     else:
-        if i != palabra2:
-            print(f"Incorrecto, tiene {e} error/es")
-            e = e + 1
-    
-    if i == palabra:#ganar///como checar que la palabra ya este completa y proceder a ganar
-        print("Correcto, la palabra es:", palabra)
-        quit()
+        print("No es un carácter válido")
         
 #perder
     if e == 7:
