5e467c0b3edb9ca64e040798829402ff30c2428c
DanielQueijeiro <111073727+DanielQueijeiro@users.noreply.github.com>
2022-09-28 11:17:46 -0500

    listado letras erroneas

--- ahorcado.py
+++ ahorcado.py
@@ -1,57 +1,54 @@
-from dataclasses import replace
 import random
-from time import sleep
-
-
-
-
-
 
 #bienvenida al juego
 def bienvenida_juego():
-    print("Bienvenido a Ahorcado, hecho por Daniel Queijeiro")
-    sleep(1)
+    print("----------------------------------------------------------------")
+    print("\n" "Bienvenido a Ahorcado, hecho por Daniel Queijeiro" "\n" )
+    print("----------------------------------------------------------------")
 
 
 bienvenida_juego()
 #lista de palabras
-lista = ["tarjeta", "pepinillo", "bocina", "codigo", "profesional"]
+lista = ["compra", "juego", "codigo", "python", "pescado"]
 diccionario = lista[random.randint(0,4)]
-diccionario = list(diccionario)
-print(diccionario)
+diccionario2= list(diccionario)
+letras_correctas=[]
+letras_incorrectas=[]
 tablero=len(diccionario)*"_"
+palabra = list(tablero)
 
 #mostrar cuantas letras tiene su palabra
-print("Su palabra contiene", "_" * len(diccionario), len(diccionario), "letras")
-sleep(1)
-print("Al alcanzar 6 errores, pierdes")
-sleep(1)
+print("Su palabra contiene", "_" * len(diccionario), len(diccionario), " letras")
 
-#set errores a 1
-e = 1
+print("Al alcanzar 6 errores, pierdes")
+
+#set errores a 0
+e = 0
 
 #adivinar y suma de errores (6 intentos)
-while e < 7:
-    print("Escriba una letra para adivinar/ Si cree saber cual es la palabra, introduzcala")
-    i = str((input()))
-    if diccionario == tablero:
-        print("ganaste")
-    elif "a" <= i <="z" and len(i) ==1:
-        if i in diccionario:
-            for index, letra_palabra in enumerate(diccionario):
-                if i == letra_palabra:
-                    tablero[index] = i
-            print("Correcto, la palabra es:", tablero)
-        elif i not in diccionario:
-                print(f"Incorrecto, tiene {e} error/es")
+while e < 6:
+    intento = str((input("Escriba una letra para adivinar:  ")))
+    print("----------------------------------------------------------------")
+    if "a" <= intento <="z" and len(intento) ==1:
+        if intento in diccionario2:
+            for index, value in enumerate(diccionario2):
+                if value == intento:
+                    palabra[index] = intento
+                if value != intento:
+                    palabra[index] = "_"
+            letras_correctas.append(intento)
+            print("Correcto, la palabra es:", palabra, "\n")
+        elif intento not in palabra:
+                letras_incorrectas.append(intento)
+                print(f"Incorrecto, tiene {e} error/es \n")
                 e = e + 1
-        
-        if i == diccionario:#ganar///como checar que la palabra ya este completa y proceder a ganar
+        print("Letras erroneas: ", letras_incorrectas)
+        if palabra == diccionario2:#ganar///como checar que la palabra ya este completa y proceder a ganar
             print("Correcto, la palabra es:", diccionario)
             quit()
     else:
-        print("No es un carácter válido")
+            print("No es un carácter válido")
         
 #perder
-    if e == 7:
+    if e == 6:
         print("Has alcanzado el maximo número de errores, perdiste")
